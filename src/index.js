25432ccbe21cbf8c6b279a1057f065d5eaf8c44a
Lisa Huang <lisa.yc.huang@gmail.com>
2017-10-10 21:26:51 -0700

    Added two examples to render in ReactDOM

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-// Code goes here
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// FIRST EXAMPLE
+
+// const title = React.createElement('h1', {}, 'My first react code');
+// const paragraph = React.createElement('p', {}, 'Writing some more HTML. Cool stuff!');
+
+// // Use an array to add multiple chidren
+// const container = React.createElement('div', {}, [title,paragraph]);
+
+// ReactDOM.render(
+//     container,
+//     document.getElementById('global')
+// );
+
+const list =
+React.createElement('div', {},
+  React.createElement('h1', {}, 'My favorite ice cream flavors'),
+  React.createElement('ul', {},
+    [
+      React.createElement('li', {className: 'brown'}, 'Chocolate'),
+      React.createElement('li', {className: 'white'}, 'Vanilla'),
+      React.createElement('li', {className: 'yellow'}, 'Banana')
+    ]
+  )
+);
+
+ReactDOM.render(
+list,
+document.getElementById('global')
+);
